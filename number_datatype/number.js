3560317133ebc8900dd0aaec0d158748d34b216a
Anjani Singh <anjanisingh953@gmail.com>
2026-04-20 07:30:53 +0530

    number datatype completed

---
+++ number_datatype/number.js
@@ -0,0 +1,9 @@
+"use strict";
+// var var_name : type = CSSMathValue;
+var age = 30;
+var oct = 0o100001;
+var hexa = 0b00001;
+var oct = 0x00001;
+var item = Number("200");
+console.log(oct + 10);
+console.log(oct + item);
---
+++ number_datatype/number.ts
@@ -0,0 +1,11 @@
+// var var_name : type = CSSMathValue;
+var age:number = 30
+
+var oct:number = 0o100001;
+var hexa:number = 0b00001;
+var oct:number = 0x00001;
+
+var item = Number("200");
+
+console.log(oct+10)
+console.log(oct+item)
