79d2b5ebc00b5a2e37478c15d7bcd355df3295f6
Anjani Singh <anjanisingh953@gmail.com>
2026-04-20 23:38:06 +0530

    type_in_typescript revision completed

---
+++ output/type_in_typescript/type.js
@@ -0,0 +1,10 @@
+"use strict";
+var studentData = {
+    fname: 'Anjani',
+    email: 'anjani123@gmail.com'
+};
+var empData = {
+    fname: 'Anjani',
+    email: 'anjani123@gmail.com'
+};
+//# sourceMappingURL=type.js.map
---
+++ type_in_typescript/type.ts
@@ -0,0 +1,11 @@
+type customDataType = { fname: string, email: string };
+
+var studentData : customDataType = {
+    fname: 'Anjani',
+    email: 'anjani123@gmail.com'
+}
+
+var empData : customDataType = {
+    fname: 'Anjani',
+    email: 'anjani123@gmail.com'
+}
