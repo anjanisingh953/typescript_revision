f9027f097f81a7c95957a56bdf3eee69cfbb7c7f
Anjani Singh <anjanisingh953@gmail.com>
2026-04-20 19:53:50 +0530

    Intersection task completed

---
+++ intersection/intersection.ts
@@ -0,0 +1,13 @@
+// type personTypeA = {name:string};
+// type personTypeB = {age:number};
+// type personTypeC = personTypeA & personTypeB;
+
+
+interface personTypeA {name:string};
+interface personTypeB {age:number};
+type personTypeC = personTypeA & personTypeB;
+
+
+var PersonDataA:personTypeA = { name: 'Anjani' };
+var PersonDataB:personTypeB = { age: 25 };
+var PersonDataC:personTypeC = { name: 'Amit', age: 24 } 
--- output/intersection/intersection.js
+++ output/intersection/intersection.js
@@ -1,2 +1,10 @@
 "use strict";
+// type personTypeA = {name:string};
+// type personTypeB = {age:number};
+// type personTypeC = personTypeA & personTypeB;
+;
+;
+var PersonDataA = { name: 'Anjani' };
+var PersonDataB = { age: 25 };
+var PersonDataC = { name: 'Amit', age: 24 };
 //# sourceMappingURL=intersection.js.map
