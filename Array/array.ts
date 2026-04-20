9f53028e5f785b33c34bca4a2a314ec340466e4a
Anjani Singh <anjanisingh953@gmail.com>
2026-04-20 14:05:10 +0530

    Array task completed

---
+++ Array/array.ts
@@ -0,0 +1,15 @@
+var users:string[] = ['anil','sachin','rohit'];
+var marks:number[] = [100,200,300,500];
+
+//second way to define an array
+var students:Array<string> = ['bruece lee','ritik','deepak'];
+
+marks.push(91500)
+console.log(marks)
+
+//define a readonly array
+var collegeName:ReadonlyArray<string> = ['VITM','IIT-Bombay','IIT-Delhi'];
+
+// collegeName.push('SDBCT'); //Error
+
+
---
+++ output/Array/array.js
@@ -0,0 +1,11 @@
+"use strict";
+var users = ['anil', 'sachin', 'rohit'];
+var marks = [100, 200, 300, 500];
+//second way to define an array
+var students = ['bruece lee', 'ritik', 'deepak'];
+marks.push(91500);
+console.log(marks);
+//define a readonly array
+var collegeName = ['VITM', 'IIT-Bombay', 'IIT-Delhi'];
+// collegeName.push('SDBCT'); //Error
+//# sourceMappingURL=array.js.map
